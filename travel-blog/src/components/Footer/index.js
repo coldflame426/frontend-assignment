bd2b7dee297c6a78208a5c1ab575016f4272e195
coldflame426 <sacred3091122@gmail.com>
2020-11-17 13:12:37 -0500

    Implement responsiveness

--- travel-blog/src/components/Footer/index.js
+++ travel-blog/src/components/Footer/index.js
@@ -6,10 +6,10 @@ const Footer = () => {
   return (
     <div className="footer__wrapper">
       <span>&#169; 2020 Travelize</span>
-      <div className="footer__navitems">
-        <span>Privacy Policy</span>
-        <span>Terms and conditions</span>
-      </div>
+      {/* <div className="footer__navitems"> */}
+      <span>Privacy Policy</span>
+      <span>Terms and conditions</span>
+      {/* </div> */}
     </div>
   );
 };
